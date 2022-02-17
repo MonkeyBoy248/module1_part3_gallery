7cdd98823e83c9b284b48e1badd02b635be7f376
Kirill Temnyi <kirik201100@gmail.com>
2022-02-17 16:49:25 +0300

    feat: add redirection to the authentication page
    add redirection to the authentication page after the token validity period expires
    
    MODULE-1-PART-3

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,6 +1,11 @@
 const galleryPhotos = document.querySelector('.gallery__photos');
 const galleryTemplate = document.querySelector('.gallery__template');
+const pagesLinksContainer = document.querySelector('.gallery__pages');
+const galleryErrorMessage = document.querySelector('.gallery__error-message');
 const galleryUrl = 'https://hjdjs55gol.execute-api.us-east-1.amazonaws.com/api/gallery';
+const loginUrl = new URL('http://127.0.0.1:5500/pages/authentication.html');
+const currentUrl = new URL(window.location.href);
+
 
 async function getPicturesData (url) {
   try {
@@ -12,15 +17,16 @@ async function getPicturesData (url) {
     })
   
     const data = await response.json();
-    console.log(data);
-    return data;
-  } catch (err) {
-    console.log(err);
+    createPictureTemplate(data);
+    setNewUrl(data);
+    galleryErrorMessage.textContent = '';
+  } catch {
+    galleryErrorMessage.textContent = 'Невозможно получить страницу с таким номером! Попробуйте выбрать другую страницу';
   }
 }
 
 function getToken () {
-  JSON.parse(localStorage.getItem('token'));
+  return JSON.parse(localStorage.getItem('token'));
 }
 
 function createPictureTemplate (pictures) {
@@ -36,8 +42,62 @@ function createPictureTemplate (pictures) {
   }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  getPicturesData(galleryUrl)
-  .then(data => createPictureTemplate(data));
+function setNewUrl (params) {
+  const newUrl = new URL(`?page=${params.page}`, currentUrl);
+  history.pushState(null, null, newUrl);
+}
+
+function deleteToken () {
+  if (Date.now() - JSON.parse(localStorage.getItem('token_timestamp')) >= 20000) {
+    localStorage.removeItem('token');
+    localStorage.removeItem('token_timestamp');
+  }
+}
+
+function redirectWhenExpires () {
+  if (!getToken()) {
+    window.location.replace(`${loginUrl}?currentPage=${currentUrl.searchParams.get('page')}`);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {  
+  if(!currentUrl.searchParams.get('page')) {
+    getPicturesData(`${galleryUrl}?page=1`);
+  }else {
+    getPicturesData(`${galleryUrl}?page=${currentUrl.searchParams.get('page')}`);
+  }
+
+  const currentActiveLink = pagesLinksContainer.querySelector('.active');
+  
+  for (let link of pagesLinksContainer.children) {
+    if (link.textContent == currentUrl.searchParams.get('page')) {
+      currentActiveLink.classList.remove('active');
+      link.classList.add('active');
+    }
+  }
+
+  redirectWhenExpires();
 })
+
+pagesLinksContainer.addEventListener('click', (e) => {
+  const currentActiveLink = pagesLinksContainer.querySelector('.active');
+  e.preventDefault();
+  if (currentActiveLink !== e.target) {
+    currentUrl.searchParams.set('page', e.target.textContent);
+    
+    getPicturesData(`${galleryUrl}?page=${currentUrl.searchParams.get('page')}`);
+    currentActiveLink.classList.remove('active');
+    e.target.classList.add('active');
+
+    redirectWhenExpires();
+  }
+})
+
+setInterval(() => {
+  deleteToken();
+  redirectWhenExpires();
+}, 300000)
+
+
+
 
