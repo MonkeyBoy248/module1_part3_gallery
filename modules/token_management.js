ab9eeaf8fc180be1efe1cb07a36f37277834d6d5
Kirill Temnyi <kirik201100@gmail.com>
2022-02-21 17:39:27 +0300

    refactor: replace string keys with variables
    replace string keys 'token' and 'token_timestamp' in localStorage functions with variables import from environment_variables file
    
    MODULE-1-PART-3

--- modules/token_management.js
+++ modules/token_management.js
@@ -1,21 +1,29 @@
+import { tokenKey, tokenTimestampKey } from "./environment_variables.js";
+
 function getToken () {
-  return JSON.parse(localStorage.getItem('token'));
+  console.log(tokenKey);
+  return JSON.parse(localStorage.getItem(tokenKey));
 }
 
 function getTokenTimestamp () {
-  return JSON.parse(localStorage.getItem('token_timestamp'));
+  return JSON.parse(localStorage.getItem(tokenTimestampKey));
 }
 
 function setToken (token) {
-  localStorage.setItem('token', JSON.stringify(token));
-  localStorage.setItem('token_timestamp', JSON.stringify(Date.now()));
+  localStorage.setItem(tokenKey, JSON.stringify(token));
+  localStorage.setItem(tokenTimestampKey, JSON.stringify(Date.now()));
 }
 
 function deleteToken () {
   if (Date.now() - getTokenTimestamp() >= 600000) {
-    localStorage.removeItem('token');
-    localStorage.removeItem('token_timestamp');
+    localStorage.removeItem(tokenKey);
+    localStorage.removeItem(tokenTimestampKey);
   }
 }
 
-export {getToken, getTokenTimestamp, setToken, deleteToken}
+export {
+  getToken, 
+  getTokenTimestamp, 
+  setToken, 
+  deleteToken
+}
